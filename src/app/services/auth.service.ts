fab7d225f91eea72e2768f3f516e4f997791d32f
Rizki Kurniawati Munawaroh <rizkikurniawatimunawaroh@gmail.com>
2025-07-03 10:53:31 +0700

    add setting proxy prod

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,18 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from '../../environment/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = environment.apiUrl;
   private apiUrl = 'http://localhost:8080/api/auth';
   private roles: string[] = [];
 
   constructor(private http: HttpClient, private router: Router) {}
 
   login(credentials: { email: string; password: string }) {
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials)
+    return this.http.post<{ token: string }>(`${this.baseUrl}/auth/login`, credentials)
       .pipe(
         tap(response => {
           localStorage.setItem('token', response.token);         
@@ -23,7 +25,7 @@ export class AuthService {
   }
 
   register(data: { name: string; email: string; password: string; roles: string[] }) {
-    return this.http.post<{ token: string }>(`${this.apiUrl}/register`, data)
+    return this.http.post<{ token: string }>(`${this.baseUrl}/auth/register`, data)
       .pipe(
         tap(response => {
           localStorage.setItem('token', response.token);
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://103.151.24.6:8080/api'
+};
